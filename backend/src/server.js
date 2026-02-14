7cbc15b7d0714a93d98b55e83cdc5429f70d6b4d
tamojit <tamojitbiswas176@gmail.com>
2026-02-14 19:21:10 +0530

    set up the server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -7,6 +8,7 @@ import messageRoutes from './routes/message.route.js';
 
 dotenv.config();
 const app=express();
+const __dirname=path.resolve();
 
 //Authentication routes
 app.use('/api/auth',authRoutes);
@@ -14,6 +16,17 @@ app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 
 
+
+
+
+if(process.env.NODE_ENV==="production")
+{
+    app.use(express.static(path.join(__dirname,'../frontend/dist')));
+    app.get('*',(_,res)=>
+    {
+        res.sendFile(path.join(__dirname,'../frontend/dist/index.html'));
+    });
+}
 const PORT=process.env.PORT || 3000;
 
 
